f7c7c69d8b2d537ab441ebd6912a3b67dbc8413b
syed <syed.quaderi@yahoo.com.au>
2019-04-12 08:48:24 +0930

    input sample binding

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+class App extends Component {
+  state = {
+    username: 'Faisal'
+  }
+
+  usernameChangedHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+        <UserInput 
+          changed={this.usernameChangedHandler} 
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="Syed" />
+      </div>
+    );
+  }
+}
+
+export default App;
